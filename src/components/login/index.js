98b1698c523fb978f8823ca3798a6d00c910f817
everbrez <everbrez@gmail.com>
2019-04-19 21:22:41 +0800

    [sidebar] add sidebar

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
 import Topbar from 'app/topbar'
-
+import Sidebar from 'app/sidebar'
 import classes from "./style.scss";
 
 function Login(props) {
   return (
     <div className={classes.root}>
       <Topbar />
+      <Sidebar />
     </div>
   )
 }
---
+++ src/components/sidebar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
+
+function Sidebar(props) {
+  return (
+    <div>
+      <SwipeableDrawer variant="permanent" open>
+        233
+      </SwipeableDrawer>
+    </div>
+  )
+}
+
+export default Sidebar
